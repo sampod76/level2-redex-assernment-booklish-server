4b104294eaf2f0386f3e3eab29a954c57c400e42
sampod76 <108483839+sampod76@users.noreply.github.com>
2023-09-21 15:01:08 +0600

    add user router

---
+++ src/app/modules/user/user.route.ts
@@ -0,0 +1,9 @@
+import express from 'express';
+import { UserController } from './user.controller';
+const router = express.Router();
+router.post(
+  '/create-user',
+  UserController.createUser
+)
+router.get('/:id', UserController.getSingleUser);
+export const UserRoutes = router;
